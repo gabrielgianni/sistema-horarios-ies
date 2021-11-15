466fc1c89ecaed6be5b5c34a4e1898c98a6ea58a
Gabriel <contatogabrielgianni@gmail.com>
2021-11-15 01:17:42 -0300

    :sparkles: conclusão da página sobre o desenvolvedor e outras modificações

---
+++ dev/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados do Desenvolvedor - UniTime</title>
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+</head>
+<body>
+    <?php require_once '../assets/partials/header.php'; ?>
+    <section class="text-center title">
+        <h1>Dados do Desenvolvedor</h1>
+    </section>
+    <main class="info-dev">
+        <p class="fs-2">Gabriel Gianni Souza Oliveira - 2088708-6</p>
+        <p class="fs-2">Universidade Cruzeiro do Sul - campus Paulista</p>
+        <br><br>
+        <p class="fs-2">Projeto para a disciplina: Estágio Supervisionado</p>
+        <p class="fs-2">Ministrado pelo Prof. Manuel Fernández Paradela Ledón</p>
+    </main>
+    <?php require_once '../assets/partials/footer.php'; ?>
+</body>
+</html>
