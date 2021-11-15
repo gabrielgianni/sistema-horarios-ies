ed013ca11a4895783c3cdaf10fcdbd3495c7a733
Gabriel <contatogabrielgianni@gmail.com>
2021-11-15 12:01:26 -0300

    :sparkles: página Horário de Disciplinas concluída

--- assets/partials/footer.php
+++ assets/partials/footer.php
@@ -1,4 +1,4 @@
-﻿<footer class="text-center">
+<footer class="text-center">
     <p>UniTime &copy; 2021 - Todos os direitos reservados.</p>
     <p>Projeto de código aberto criado por <a href="https://github.com/gabrielgianni" title="Gabriel Gianni - GitHub" target="_blank" class="linkFooter">Gabriel Gianni</a>, acesse o código fonte no <a href="https://github.com/gabrielgianni/sistema-horarios-ies/" target="_blank" class="linkFooter">GitHub</a>.</p>
     <div class="btn_dev">
--- disciplinas/pesquisar.php
+++ disciplinas/pesquisar.php
@@ -1,29 +1,51 @@
 <?php
     include_once "../connection.php";
-
     $disciplina = mb_strtoupper(trim($_POST['disciplina']), "UTF-8");
-
     $result_disciplinas = "SELECT * FROM horarios WHERE CONCAT(discSegM, discSegN, discTerM, discTerN, discQuaM, discQuaN, discQuiM, discQuiN, discSexM, discSexN) LIKE '%$disciplina%'";
 
     $result = mysqli_query($conn, $result_disciplinas);
 
     $numRegistros = mysqli_num_rows($result);
-
-    echo "Ofertas da disciplina: " . $disciplina . "<br><br>";
-    if ($numRegistros != 0) {
-        while($linha = mysqli_fetch_array($result)) {
-            echo "Segunda, manhã, " . $linha['discSegM'] . " " . $linha['segM'] . ", prof.: " . $linha['nomeProf'] . "<br>";
-            echo "Segunda, noite, " . $linha['discSegN'] . " " . $linha['segN'] . ", prof.: " . $linha['nomeProf'] . "<br>";
-            echo "Terça, manhã, " . $linha['discTerM'] . " " . $linha['terM'] . ", prof.: " . $linha['nomeProf'] . "<br>";
-            echo "Terça, noite, " . $linha['discTerN'] . " " . $linha['terN'] . ", prof.: " . $linha['nomeProf'] . "<br>";
-            echo "Quarta, manhã, " . $linha['discQuaM'] . " " . $linha['quaM'] . ", prof.: " . $linha['nomeProf'] . "<br>";
-            echo "Quarta, noite, " . $linha['discQuaN'] . " " . $linha['quaN'] . ", prof.: " . $linha['nomeProf'] . "<br>";
-            echo "Quinta, manhã, " . $linha['discQuiM'] . " " . $linha['quiM'] . ", prof.: " . $linha['nomeProf'] . "<br>";
-            echo "Quarta, noite, " . $linha['discQuiN'] . " " . $linha['quiN'] . ", prof.: " . $linha['nomeProf'] . "<br>";
-            echo "Sexta, manhã, " . $linha['discSexM'] . " " . $linha['sexM'] . ", prof.: " . $linha['nomeProf'] . "<br>";
-            echo "Sexta, noite, " . $linha['discSexN'] . " " . $linha['sexN'] . ", prof.: " . $linha['nomeProf'] . "<br>";
-        }
-    } else {
-        echo "Nenhuma disciplina foi encontrada com a palavra ".$disciplina."";
-    }
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado da pesquisa: <?php echo $disciplina; ?> - UniTime</title>
+    <link rel="icon" type="image/x-icon" href="/assets/img/teacher.png">
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+</head>
+<body>
+    <?php require_once '../assets/partials/header.php'; ?>
+    <section class="text-center title">
+        <h1>Horário de Disciplinas</h1>
+    </section>
+    <main class="container mt-5">
+        <?php
+        echo "<p class='fw-bold'>Ofertas da disciplina: " . $disciplina . "</p>";
+        if ($numRegistros != 0) {
+            while ($linha = mysqli_fetch_array($result)) {
+                echo "<p>Segunda, manhã, " . $linha['discSegM'] . " " . $linha['segM'] . ", prof.: " . $linha['nomeProf'] . "</p>";
+                echo "<p>Segunda, noite, " . $linha['discSegN'] . " " . $linha['segN'] . ", prof.: " . $linha['nomeProf'] . "</p>";
+                echo "<p>Terça, manhã, " . $linha['discTerM'] . " " . $linha['terM'] . ", prof.: " . $linha['nomeProf'] . "</p>";
+                echo "<p>Terça, noite, " . $linha['discTerN'] . " " . $linha['terN'] . ", prof.: " . $linha['nomeProf'] . "</p>";
+                echo "<p>Quarta, manhã, " . $linha['discQuaM'] . " " . $linha['quaM'] . ", prof.: " . $linha['nomeProf'] . "</p>";
+                echo "<p>Quarta, noite, " . $linha['discQuaN'] . " " . $linha['quaN'] . ", prof.: " . $linha['nomeProf'] . "</p>";
+                echo "<p>Quinta, manhã, " . $linha['discQuiM'] . " " . $linha['quiM'] . ", prof.: " . $linha['nomeProf'] . "</p>";
+                echo "<p>Quarta, noite, " . $linha['discQuiN'] . " " . $linha['quiN'] . ", prof.: " . $linha['nomeProf'] . "</p>";
+                echo "<p>Sexta, manhã, " . $linha['discSexM'] . " " . $linha['sexM'] . ", prof.: " . $linha['nomeProf'] . "</p>";
+                echo "<p>Sexta, noite, " . $linha['discSexN'] . " " . $linha['sexN'] . ", prof.: " . $linha['nomeProf'] . "</p><br>";
+            }
+        } else {
+            echo "Nenhuma disciplina foi encontrada com a palavra " . $disciplina . "";
+        }
+        ?>
+    </main>
+    <?php require_once '../assets/partials/footer.php'; ?>
+</body>
+</html>
